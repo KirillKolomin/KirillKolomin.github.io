42c8f887c182ae47c8330b616f18d8c88e2e19f7
kirill kolomin <kirillvintorez@gmail.com>
2024-05-29 11:19:06 +0500

    add login page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import LoginForm from "@exchange-gateway/widgets/login-form/LoginForm";
+import './page.css'
+
+const LoginPage = () => {
+    return <main>
+        <LoginForm />;
+    </main>
+};
+
+export default LoginPage;
