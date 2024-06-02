c0b5805c0e862d3346f9b7166430b612f564e105
kirill kolomin <kirillvintorez@gmail.com>
2024-06-02 18:47:33 +0500

    Prettify get session

--- src/shared/authentication/get-session.ts
+++ src/shared/authentication/get-session.ts
@@ -4,11 +4,11 @@ import { cookies } from 'next/headers';
 import { AUTH_COOKIE } from '@exchange-gateway/shared/authentication/consts';
 import { decrypt } from '@exchange-gateway/shared/authentication/encrypt';
 
-export const getSession = async (): Promise<unknown | null> => {
+export default async function getSession(): Promise<unknown | null> {
   const session = cookies().get(AUTH_COOKIE)?.value;
 
   if (!session) {
     return null;
   }
   return decrypt(session);
-};
+}
