1bbc36674f3388174d38147aa0690e86671fde74
kirill kolomin <kirillvintorez@gmail.com>
2024-06-05 01:29:30 +0500

    Refactoring

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
-import PersonalInformationCard
-  from '@exchange-gateway/shared/components/pesonal-information-card/PersonalInformationCard';
 import { GET as getProfile, User } from '@exchange-gateway/app/api/profile/route';
 import NavigationBar from '@exchange-gateway/widgets/navigation-bar/NavigationBar';
+import InformationCard, {
+  InformationCardStatements,
+} from '@exchange-gateway/shared/components/information-card/InformationCard';
+import { personalInformationTranslations } from '@exchange-gateway/shared/translations/personal-information';
 
 export default async function Home() {
   const response: Response = await getProfile();
   const user: User = await response.json();
+  const informationCardStatements: InformationCardStatements = {
+    fullName: [personalInformationTranslations.fullName, user.fullName],
+    phone: [personalInformationTranslations.phone, user.phone],
+  };
 
   return (
     <>
       <header><NavigationBar /></header>
       <main>
-        <PersonalInformationCard personalInformation={user} />
+        <InformationCard title={personalInformationTranslations.cardTitle} description={personalInformationTranslations.cardDescription} statements={informationCardStatements} />
       </main>
     </>
   );
