9c048d683697df182d935eab1e1cff6e1cd67503
kirill kolomin <kirillvintorez@gmail.com>
2024-06-05 01:25:37 +0500

    Increase session time

--- src/shared/api/authentication/consts.ts
+++ src/shared/api/authentication/consts.ts
@@ -1,4 +1,4 @@
 export const AUTH_COOKIE = 'session';
 export const REDIRECT_URL_QUERY = 'redirectUrl';
 export const ENCRYPTION_KEY = 'secret';
-export const SESSION_TIME_IN_SEC = 30;
+export const SESSION_TIME_IN_SEC = 300;
