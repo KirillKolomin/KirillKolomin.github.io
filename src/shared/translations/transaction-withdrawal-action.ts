33ed7d203eea05dad0ed8ae79973fd715552d779
kirill kolomin <kirillvintorez@gmail.com>
2024-06-05 01:28:30 +0500

    Add translations

---
+++ src/shared/translations/transaction-withdrawal-action.ts
@@ -0,0 +1,6 @@
+export const transactionWithdrawalAction = {
+  typeError: 'Сумма должна иметь тип числа',
+  negativeNumberError: 'Сумма должна быть числом больше 0',
+  balanceMoreThanCurrentError: 'Сумма не может быть больше вашего баланса',
+  unableToMakeTransaction: 'Мы не смогли провести операцию вывода средств, пожалуйста, попробуйте позже',
+};
